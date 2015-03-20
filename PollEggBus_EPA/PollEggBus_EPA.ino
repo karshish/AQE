64514499bc2174a1eb91ac208cffa1d4a3df0ac8
Michael van den Bossche <mv7b@virginia.edu>
2015-03-19 21:38:18 -0400

    Added code for data logging to SD card

--- PollEggBus_EPA/PollEggBus_EPA.ino
+++ PollEggBus_EPA/PollEggBus_EPA.ino
@@ -1,17 +1,42 @@
 /* Sketch for using the AQE shield with Arduino Uno
    Copied from: https://github.com/jmsaavedra/Air-Quality-Egg/tree/master/libraries/EggBus
+   Adapted by Michael van den Bossche
+   Version 2.0 - included writing to SD card using the Fat16 library.
 */
 
 #include <stdint.h>
 #include <DHT.h>
 #include "Wire.h"
 #include "EggBus.h"
+#include <Fat16.h>       // for SD disk
+#include <Fat16util.h>   // for SD disk
 
 EggBus eggBus;
+
+//define filename
+char name [13] = "AQE001.CSV";
+String outputString;
 
 #define DHTPIN 17 //analog pin 3
 #define DHTTYPE DHT22  
 DHT dht(DHTPIN, DHTTYPE);
+
+SdCard card;         
+Fat16 file; 
+
+// store error strings in flash to save RAM
+// from Stalkerv21_DataLogger_5min example
+#define error(s) error_P(PSTR(s))
+
+void error_P(const char* str) {
+  PgmPrint("error: ");
+  SerialPrintln_P(str);
+  if (card.errorCode) {
+    PgmPrint("SD error: ");
+    Serial.println(card.errorCode, HEX);
+  }
+  while(1);
+}
 
 void setup(){
   Serial.begin(9600);
@@ -19,6 +44,29 @@ void setup(){
   Serial.println(F("======================================================================\r\n"));
   Serial.println(F("Timestamp, Sensor Type, Sensor Value, Sensor Units, Sensor Resistance"));
   Serial.println(F("----------------------------------------------------------------------"));  
+
+  // from Stalkerv21_DataLogger_5min example
+  // initialize the SD card
+  if (!card.init()) error("card.init");
+   
+  // initialize a FAT16 volume
+  if (!Fat16::init(&card)) error("Fat16::init");
+  
+  // clear write error
+  file.writeError = false;
+  
+  // O_CREAT - create the file if it does not exist
+  // O_APPEND - seek to the end of the file prior to each write
+  // O_WRITE - open for write
+  if (!file.open(name, O_CREAT | O_APPEND | O_WRITE))
+      error("error opening file");
+  
+  // logging header
+  file.println("timestamp [ms?],[NO2] [ppb],[CO] [ppb],[O3] [ppb],PM,RH [%RH],T [oC]");
+    
+  if (!file.close()) 
+      error("error closing file");
+
 }
 
 void loop(){
@@ -27,7 +75,10 @@ void loop(){
   float sensor_resistance;
   
   eggBus.init();
-    
+
+  outputString = Float2String(millis());
+  outputString += ',';
+  
   while((egg_bus_address = eggBus.next())){
     uint8_t numSensors = eggBus.getNumSensors();
     for(uint8_t ii = 0; ii < numSensors; ii++){
@@ -41,7 +92,9 @@ void loop(){
       sensor_resistance = eggBus.getSensorResistance(ii);
       
       Serial.print(eggBus.getSensorValue(ii), 8);          
-      Serial.print(F(", "));      
+      Serial.print(F(", "));
+      outputString += Float2String(eggBus.getSensorValue(ii));
+      outputString += ','; 
 
       Serial.print(eggBus.getSensorUnits(ii));            
       Serial.print(F(", "));      
@@ -64,7 +117,8 @@ void loop(){
   Serial.println(F("%, n/a"));
   delay(2500);
 
-
+  outputString += Float2String(currHumidity);
+  outputString += ',';
 
   Serial.print(millis(), DEC);
   Serial.print(F(", "));      
@@ -78,7 +132,23 @@ void loop(){
   Serial.println(F("degrees C, n/a"));
   delay(2500);
 
+  outputString += Float2String(currentTemperature);
   Serial.println();
+
+  // clear write error
+  file.writeError = false;
+  
+  // O_CREAT - create the file if it does not exist
+  // O_APPEND - seek to the end of the file prior to each write
+  // O_WRITE - open for write
+  if (!file.open(name, O_CREAT | O_APPEND | O_WRITE))
+    error("error opening file");  
+
+  file.println(outputString);
+  
+  if (!file.close()) 
+    error("error closing file");
+ 
 }
 
 void printAddress(uint8_t * address){
@@ -117,3 +187,13 @@ float getTemperature(){
   } 
   return t;
 }
+
+String Float2String(const float& ff)
+{
+  //ugh...float to String is a pain in Arduino World...
+  char tempStr[32];
+  dtostrf(ff, 4, 2, tempStr);
+
+  return String(tempStr);
+}
+
