8ee1b4eaa7d40ec60ab983c7f7ae320626645708
Michael van den Bossche <mv7b@virginia.edu>
2015-03-19 23:18:34 -0400

    Added code for RTC

--- PollEggBus_EPA/PollEggBus_EPA.ino
+++ PollEggBus_EPA/PollEggBus_EPA.ino
@@ -1,10 +1,10 @@
 /* Sketch for using the AQE shield with Arduino Uno
    Copied from: https://github.com/jmsaavedra/Air-Quality-Egg/tree/master/libraries/EggBus
    Adapted by Michael van den Bossche
+   Version 5.0 - included RTC to keep time.
    Version 4.0 - updated the SD card library to use fat32 for cards > 4GB. Library made by Bill Greiman
    Version 3.0 - changed the library for the DHT22 - now using the library by Rob Tillaart: https://github.com/RobTillaart/Arduino
    Version 2.0 - included writing to SD card using the Fat16 library.
-
 */
 
 #include <stdint.h>
@@ -12,6 +12,7 @@
 #include "Wire.h"
 #include "EggBus.h"
 #include <SdFat.h>          // include the SdFat library
+#include <DS3231.h>         // real-time clock
 
 EggBus eggBus;
 
@@ -24,6 +25,10 @@ dht DHT;
 #define DHT22_PIN A3 //analog pin 3
 //end portion from dht22_test
 
+//Create the RTC object
+DS3231 RTC; 
+DateTime dt(__DATE__,__TIME__);
+
 SdFat sd;                   // Start the SdFat object, call it 'sd' (could be called anything).
 SdFile myFile;              // start the SdFile object, call it 'myFile'.
 
@@ -33,6 +38,11 @@ SdFile myFile;              // start the SdFile object, call it 'myFile'.
 
 void setup(){
   Serial.begin(9600);
+  
+  Wire.begin();
+  RTC.begin();
+  // RTC.adjust(dt);  
+  
   Serial.println(F("Air Quality Egg - EPA Serial Build v1.0"));
   Serial.println(F("======================================================================\r\n"));
   Serial.println(F("Timestamp, Sensor Type, Sensor Value, Sensor Units, Sensor Resistance"));
@@ -54,82 +64,97 @@ void setup(){
   myFile.close();
 }
 
-void loop(){
-  uint8_t   egg_bus_address;
-  uint32_t r0 = 0;
-  float sensor_resistance;
-  
-  eggBus.init();
-
-  outputString = Float2String(millis());
-  outputString += ',';
-  
-  while((egg_bus_address = eggBus.next())){
-    uint8_t numSensors = eggBus.getNumSensors();
-    for(uint8_t ii = 0; ii < numSensors; ii++){
-      Serial.print(millis(), DEC);
-      Serial.print(F(", "));      
-      
-      Serial.print(eggBus.getSensorType(ii));
-      Serial.print(F(", "));
-      
-      r0 = eggBus.getSensorR0(ii);
-      sensor_resistance = eggBus.getSensorResistance(ii);
-      
-      Serial.print(eggBus.getSensorValue(ii), 8);          
-      Serial.print(F(", "));
-      outputString += Float2String(eggBus.getSensorValue(ii));
-      outputString += ','; 
-
-      Serial.print(eggBus.getSensorUnits(ii));            
-      Serial.print(F(", "));      
-      
-      Serial.print(sensor_resistance, 3);
-      
-      Serial.println();
-    }
+//allows us to read the sensors every second, regardless of how much time we've spent doing other things
+DateTime lastSensorRead;
+boolean OneSecondHasPassed(void)
+{
+  DateTime rightNow = RTC.now();
+  if(rightNow.get() >= lastSensorRead.get() + 6) //egads, who is the incompetent who wrote the RTC library?
+  {
+    lastSensorRead = rightNow;
+    return true;
   }
-  uint32_t start = micros();
-  int chk = DHT.read22(DHT22_PIN);
-  uint32_t stop = micros();
-  
-  Serial.print(millis(), DEC);
-  Serial.print(F(", "));      
-  
-  Serial.print("Humidity");
-  Serial.print(F(", "));
-  float currHumidity = DHT.humidity;
-  Serial.print(currHumidity, 8);
-  Serial.print(F(", "));      
-
-  Serial.println(F("%, n/a"));
-  delay(2500);
-
-  outputString += Float2String(currHumidity);
-  outputString += ',';
-
-  Serial.print(millis(), DEC);
-  Serial.print(F(", "));      
-  
-  Serial.print("Temperature");
-  Serial.print(F(", "));
-  float currentTemperature = DHT.temperature;
-  Serial.print(currentTemperature, 8);
-  Serial.print(F(", "));      
-
-  Serial.println(F("degrees C, n/a"));
-  delay(2500);
-
-  outputString += Float2String(currentTemperature);
-  Serial.println();
-
-  if (!sd.begin(10, SPI_HALF_SPEED)) sd.initErrorHalt();
-  if (!myFile.open(name, O_RDWR | O_CREAT | O_AT_END)) sd.errorHalt();
-  myFile.println(outputString);
-  
-  myFile.close(); 
+  else return false;
 }
 
+void loop(){
+  if (OneSecondHasPassed())
+     {  
+    uint8_t   egg_bus_address;
+    uint32_t r0 = 0;
+    float sensor_resistance;
+  
+    eggBus.init();
+
+    outputString = Float2String(millis());
+    outputString += ',';
+  
+    while((egg_bus_address = eggBus.next())){
+      uint8_t numSensors = eggBus.getNumSensors();
+      for(uint8_t ii = 0; ii < numSensors; ii++){
+        Serial.print(millis(), DEC);
+        Serial.print(F(", "));      
+      
+        Serial.print(eggBus.getSensorType(ii));
+        Serial.print(F(", "));
+      
+        r0 = eggBus.getSensorR0(ii);
+        sensor_resistance = eggBus.getSensorResistance(ii);
+      
+        Serial.print(eggBus.getSensorValue(ii), 8);          
+        Serial.print(F(", "));
+        outputString += Float2String(eggBus.getSensorValue(ii));
+        outputString += ','; 
+
+        Serial.print(eggBus.getSensorUnits(ii));            
+        Serial.print(F(", "));      
+      
+        Serial.print(sensor_resistance, 3);
+      
+        Serial.println();
+      }
+    }
+    uint32_t start = micros();
+    int chk = DHT.read22(DHT22_PIN);
+    uint32_t stop = micros();
+  
+    Serial.print(millis(), DEC);
+    Serial.print(F(", "));      
+  
+    Serial.print("Humidity");
+    Serial.print(F(", "));
+    float currHumidity = DHT.humidity;
+    Serial.print(currHumidity, 8);
+    Serial.print(F(", "));      
+
+    Serial.println(F("%, n/a"));
+    delay(2500);
+
+    outputString += Float2String(currHumidity);
+    outputString += ',';
+
+    Serial.print(millis(), DEC);
+    Serial.print(F(", "));      
+  
+    Serial.print("Temperature");
+    Serial.print(F(", "));
+    float currentTemperature = DHT.temperature;
+    Serial.print(currentTemperature, 8);
+    Serial.print(F(", "));      
+
+    Serial.println(F("degrees C, n/a"));
+    delay(2500);
+
+    outputString += Float2String(currentTemperature);
+    Serial.println();
+
+    if (!sd.begin(10, SPI_HALF_SPEED)) sd.initErrorHalt();
+    if (!myFile.open(name, O_RDWR | O_CREAT | O_AT_END)) sd.errorHalt();
+    myFile.println(outputString);
+  
+    myFile.close(); 
+  }
+}
 void printAddress(uint8_t * address){
   for(uint8_t jj = 0; jj < 6; jj++){
     if(address[jj] < 16) Serial.print("0");
